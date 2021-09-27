843ed81b7582e7f8bd91fbe06885b340329aeff5
Nathan Dahlberg <58701810+nadahlberg@users.noreply.github.com>
2021-09-27 14:57:28 -0400

    add table count feedback

--- clearinghouse/views.py
+++ clearinghouse/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 def index_view(response):
-	return render(response, 'clearinghouse/index.html')
+	num_people = Person.objects.all().count()
+	num_courts = Court.objects.all().count()
+	try:
+		num_people_courts = PersonCourt.objects.all().count()
+	except:
+		num_people_courts = 0
+	return render(response, 'clearinghouse/index.html', {
+		'num_people': num_people,
+		'num_courts': num_courts,
+		'num_people_courts': num_people_courts,
+	})
 
 
 
